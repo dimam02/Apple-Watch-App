41d860ec9891255949c8d08e13ce78cbfe20f216
dimam02 <109684406+dimam02@users.noreply.github.com>
2023-06-07 02:38:56 +0300

    Initial Commit

---
+++ Progress App Watch App/Progress_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Progress_AppApp.swift
+//  Progress App Watch App
+//
+//  Created by Dmytro on 07.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct Progress_App_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Progress App/Progress_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Progress_AppApp.swift
+//  Progress App
+//
+//  Created by Dmytro on 07.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct Progress_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
