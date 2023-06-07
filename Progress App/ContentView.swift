4c0b573eeb97f7b959d8af772c179fbaccfa2857
dimam02 <109684406+dimam02@users.noreply.github.com>
2023-06-07 04:01:57 +0300

    Init commit

--- Progress App/ContentView.swift
+++ Progress App/ContentView.swift
@@ -6,16 +6,66 @@
 //
 
 import SwiftUI
+import UserNotifications
 
 struct ContentView: View {
+    @State private var progress: Double = 0.0
+    @State private var timeLeft: TimeInterval = 0.0
+    
+    let milestoneValues: [Double] = [0.5, 0.8, 1.0] // Milestone completion values
+    
+    func updateProgress() {
+        // Update progress logic based on user actions
+        progress += 0.1 // Example: Increment progress by 0.1 for demonstration
+        
+        // Check if any milestone reached and schedule notification
+        for milestone in milestoneValues {
+            if progress >= milestone && progress - 0.1 < milestone {
+                scheduleNotification(milestone: milestone)
+            }
+        }
+        
+        // Calculate and update time left
+        let totalTime: TimeInterval = 10 * 60 // Example: Total time in seconds
+        timeLeft = (1 - progress) * totalTime
+    }
+    
+    func scheduleNotification(milestone: Double) {
+        let notificationContent = UNMutableNotificationContent()
+        notificationContent.title = "Milestone Reached!"
+        notificationContent.body = "You have reached \(Int(milestone * 100))% completion."
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: notificationContent, trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(request) { error in
+            if let error = error {
+                print("Error scheduling notification: \(error)")
+            }
+        }
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Progress: \(Int(progress * 100))%")
+                .font(.title)
+                .padding()
+            
+            Button(action: {
+                updateProgress()
+            }) {
+                Text("Update Progress")
+                    .font(.headline)
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+            
+            Text("Time Left: \(Int(timeLeft / 60)) minutes")
+                .font(.headline)
+                .padding()
         }
-        .padding()
     }
 }
 
